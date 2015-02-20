c388fb938bcdac530addfbdc7bacfe1bb869215e
Shiplayer <lllytep199515@yandex.ru>
2015-02-20 13:01:02 +0300

    Server v1

--- src/ru/ifmo/Server.java
+++ src/ru/ifmo/Server.java
@@ -1,7 +1,29 @@
 package ru.ifmo;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Created by Freemahn on 20.02.2015.
  */
 public class Server {
+    ServerSocket serverSocket;
+
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("port " + port + " is closed");
+        }
+    }
+
+    public void start(){
+        while(true){
+            try {
+                Client client = new Client(serverSocket.accept());
+            } catch (IOException e) {
+                System.err.println("Client is unavailable :(");
+            }
+        }
+    }
 }
---
+++ src/ru/ifmo/ServerThread.java
@@ -0,0 +1,17 @@
+package ru.ifmo;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+
+/**
+ * Created by anton on 20.02.15.
+ */
+public class ServerThread extends Thread {
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public ServerThread(ServerSocket ss){
+
+    }
+}
