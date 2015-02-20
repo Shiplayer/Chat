a36316d0b2c5a821c243057863601f091cdf76e5
Freemahn <gordon.pav@gmail.com>
2015-02-20 13:04:42 +0300

    test branch freemahn

--- src/ru/ifmo/Server.java
+++ src/ru/ifmo/Server.java
@@ -4,4 +4,7 @@ package ru.ifmo;
  * Created by Freemahn on 20.02.2015.
  */
 public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server started");
+    }
 }
